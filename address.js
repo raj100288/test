a4653d08c5468d4d35dc01e4fb2fd4890ad1b8f9
rajvinoth <rajvinoth@armadagroupco.com>
2019-09-28 12:09:50 +0300

    newtest

--- address.js
+++ address.js
@@ -6,7 +6,7 @@
 	},
 	
 	//mapValuesJson: function()
-	{
+	/*{
 		var mapvalues = {
 			street_number: 'short_name',
 			route: 'short_name',
@@ -22,7 +22,7 @@
 			};
 		
 		return mapvalues;
-	},
+	},*/
 
 	regionBasedOnCountry: function()
 	{
